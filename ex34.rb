e515f40db31c4a46619e4e2d31b7c339505c45a7
SeuRAUL <kenny.is.inmortal@gmail.com>
2013-05-26 11:19:14 -0300

    34 e 35

---
+++ ex34.rb
@@ -0,0 +1,10 @@
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+puts "The animal at 1 is the second animal and is a #{animals[1]}."
+puts "The 3th animal is at 2 and is a #{animals[2]}."
+puts "The 1st animal is at 0 and is a #{animals.first}."
+puts "The animal at 3 is the 4th animal and is a #{animals[3]}."
+puts "The 5st animal is at 4 and is a #{animals[4]}."
+puts "The animal at 2 is the 3th animal and is a #{animals[2]}."
+puts "The 6st animal is at 5 and is a #{animals[5]}."
+puts "The animal at 4 is the 5th animal and is a #{animals[4]}."
