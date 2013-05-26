9ed58d4df6b2da14b4410301524bff3756756dd3
SeuRAUL <kenny.is.inmortal@gmail.com>
2013-05-26 12:07:37 -0300

    melhora de verificação na coleta de gold

--- ex35.rb
+++ ex35.rb
@@ -8,9 +8,7 @@ def gold_room()
 	puts "This room is full of gold. How much do you take?"
 
 	prompt; next_move = gets.chomp()
-	if next_move.include? "0" or next_move.include? "1"
-		how_much = next_move.to_i()
-	else
+	if not how_much = next_move.to_i()
 		dead("Man, learn to type a number.")
 	end
 
